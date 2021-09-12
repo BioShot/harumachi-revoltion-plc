1cc3b6fec578b2906b7c89b3c9b808d9fabae274
BioShot <86807700+BioShot@users.noreply.github.com>
2021-09-12 18:48:34 -0400

    Update and rename haru.bat to msg.vbs

--- vbs/haru.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo starting...
-pause 
-START https://www.youtube.com/watch?v=BjDebmqFRuc
-echo PRANKED HAHAHA LOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
-START C:\Users\Levi\Documents\Bio-Shot\upd.vbs  
-START C:\Users\Levi\Documents\Bio-Shot\msg.vbs
-
-cls
