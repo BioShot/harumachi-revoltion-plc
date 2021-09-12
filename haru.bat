fc3b9b5e4d5af6a7bfe036c3363184701bbf1c17
BioShot <86807700+BioShot@users.noreply.github.com>
2021-09-12 19:29:22 -0400

    Update haru.bat

--- haru.bat
+++ haru.bat
@@ -3,7 +3,7 @@ echo starting...
 pause 
 START https://www.youtube.com/watch?v=BjDebmqFRuc
 echo PRANKED HAHAHA LOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
-START C:\Users\Levi\Documents\Bio-Shot\upd.vbs  
-START C:\Users\Levi\Documents\Bio-Shot\msg.vbs
+START %USERProfile%\Documents\Bio-Shot\harumachi-revoltion-plc\vbs\msg.vbs
+START %USERProfile%\Documents\Bio-Shot\harumachi-revoltion-plc\upd.vbs
 
 cls
