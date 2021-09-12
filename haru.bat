c28e32c8e4c9afc4ee03d3b44de5d93215ecb771
BioShot <86807700+BioShot@users.noreply.github.com>
2021-09-12 18:18:47 -0400

    Create haru.bat

---
+++ haru.bat
@@ -0,0 +1,9 @@
+@echo off
+echo starting...
+pause 
+START https://www.youtube.com/watch?v=BjDebmqFRuc
+echo PRANKED HAHAHA LOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+START C:\Users\Levi\Documents\Bio-Shot\upd.vbs  
+START C:\Users\Levi\Documents\Bio-Shot\msg.vbs
+
+cls
